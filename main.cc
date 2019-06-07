a505a87be09e5c853de10b313b2366d5bac14055
claireyy23 <fancy51116@gmail.com>
2019-06-07 18:32:39 +0800

    main.cc

---
+++ main.cc
@@ -0,0 +1,20 @@
+/* 
+    Your main program goes here
+*/
+#include <iostream>
+#include "network_manager.h"
+#include "gplot.h"
+#include "path.h"
+
+using namespace std;
+
+// create NetworkManager first
+NetworkManager *nm = new NetworkManager();
+
+int main(int argc, char** argv){
+
+ cout<<"HELLO CLAIRE"<<endl;
+    /* start your program */
+
+    return 0;
+}
